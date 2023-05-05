71924c38433995b43a0e2e55d787ec0fa473a650
Blair Hall <blair.hall.og@gmail.com>
2023-05-05 16:52:16 -0400

    Update

--- password_manager.py
+++ password_manager.py
@@ -15,22 +15,23 @@ def load_key():
     file.close()
     return key
 
-key = load_key()
 master_pwd = input("What is the master password? ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
    with open('passwords.txt', 'r') as f:
        for line in f.readlines():
            data = line.rstrip()
            user, passw = data.split("|")
-           print("User:", user, "Password:", passw)
+           print("User:", user, "Password:", str(fer.decrypt(passw.encode())))
 
 def add():
     name = input('Account Name: ')
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ")
